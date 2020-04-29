cde67e1abf936f5753b81990490db52489f2962c
José García Máñez <josegarciamanez@gmail.com>
2020-04-30 00:48:37 +0200

    add particles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,27 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
+import Particles from "react-particles-js"
 
 export default () => (
   <Layout>
     <SEO title="Home" />
 
     <div className="container">
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 80,
+            },
+            line_linked: {
+              color: "#000",
+              opacity: 0.1,
+              zIndex: -1000,
+            },
+          },
+        }}
+      />
       <h1>PORTFOLIO</h1>
 
       <div className="frame__info">
